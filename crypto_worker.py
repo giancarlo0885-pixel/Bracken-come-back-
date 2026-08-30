634860eb95859118cc026bdb2c8d31a6750ebf34
giancarlo0885-pixel <giancarlo0885@gmail.com>
2026-08-30 16:28:52 -0700

    Install fee-correct FIFO policy

--- crypto_worker.py
+++ crypto_worker.py
@@ -5,6 +5,7 @@ import market_worker
 from crypto_execution_guard import install_crypto_execution_quote_guard
 from paper_execution_accounting import install_paper_execution_accounting
 from paper_execution_reality import install_paper_execution_reality
+from paper_fee_policy import install_fee_aware_fifo_policy
 from structured_logging import configure_structured_logging
 
 
@@ -13,6 +14,7 @@ install_crypto_execution_quote_guard(market_worker)
 if os.getenv("EXECUTION_MODE", "paper").strip().lower() == "paper":
     install_paper_execution_reality(market_worker)
     install_paper_execution_accounting(market_worker)
+    install_fee_aware_fifo_policy()
 
 
 if __name__ == "__main__":
