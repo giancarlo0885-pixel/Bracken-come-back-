b272c5c8432092d9850d324e7cf6b3cc27d15706
giancarlo0885-pixel <giancarlo0885@gmail.com>
2026-08-30 16:29:03 -0700

    Install fee-correct FIFO policy in compatibility worker

--- worker.py
+++ worker.py
@@ -11,6 +11,7 @@ import os
 import market_worker
 from paper_execution_accounting import install_paper_execution_accounting
 from paper_execution_reality import install_paper_execution_reality
+from paper_fee_policy import install_fee_aware_fifo_policy
 from portfolio_valuation import install_closed_market_valuation_pulse
 
 
@@ -28,4 +29,5 @@ if __name__ == "__main__":
     if os.getenv("EXECUTION_MODE", "paper").strip().lower() == "paper":
         install_paper_execution_reality(market_worker)
         install_paper_execution_accounting(market_worker)
+        install_fee_aware_fifo_policy()
     market_worker.run_worker(market)
