d1f411e165a2ed72e3ec6b38cdbbbc4b5cea4911
giancarlo0885-pixel <giancarlo0885@gmail.com>
2026-08-31 06:17:02 -0700

    Install runtime integrity corrections in stock worker

--- stock_worker.py
+++ stock_worker.py
@@ -6,10 +6,12 @@ from paper_execution_accounting import install_paper_execution_accounting
 from paper_execution_reality import install_paper_execution_reality
 from paper_fee_policy import install_fee_aware_fifo_policy
 from portfolio_valuation import install_closed_market_valuation_pulse
+from runtime_integrity_patch import install_runtime_integrity_patch
 from stock_execution_repair import install_stock_execution_quote_repair
 from structured_logging import configure_structured_logging
 
 configure_structured_logging(os.getenv("LOG_LEVEL", "INFO"))
+install_runtime_integrity_patch(market_worker)
 install_closed_market_valuation_pulse(market_worker)
 install_stock_execution_quote_repair(market_worker)
 if os.getenv("EXECUTION_MODE", "paper").strip().lower() == "paper":
